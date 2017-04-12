486a65e134396fde03ba15d8a25587c3a9060407
Caitlin Cassidy <ccassidy@mit.edu>
2017-04-11 22:59:37 -0400

    added space between icon and text

--- js/load-menubar.js
+++ js/load-menubar.js
@@ -1,4 +1,3 @@
-// TODO: A bit more space between the icon and word would look better
 // TODO: Make sure the icon next to the menu and on the actual page match
 //    (only a problem for feedback I think -- page has pencil, menu has stats)
 
@@ -6,35 +5,35 @@ $(document).ready(function() {
   var menubarString = `
   <ul class="nav nav-pills nav-stacked">
     <li role="presentation">
-      <a href="index.html">
+      <a class="menu-link" href="index.html">
         <span class="glyphicon glyphicon-calendar"></span>
         Events
       </a>
     </li>
 
     <li role="presentation">
-      <a href="tasks.html">
+      <a class="menu-link" href="tasks.html">
         <span class="glyphicon glyphicon-list"></span>
         Tasks
       </a>
     </li>
 
         <li role="presentation">
-      <a href="availabilities.html">
+      <a class="menu-link" href="availabilities.html">
         <span class="glyphicon glyphicon-user"></span>
         Availabilities
       </a>
     </li>
 
         <li role="presentation">
-      <a href="feedback.html">
+      <a class="menu-link" href="feedback.html">
         <span class="glyphicon glyphicon-stats"></span>
         Feedback
       </a>
     </li>
 
     <li role="presentation">
-      <a href="notes.html">
+      <a class="menu-link" href="notes.html">
         <span class="glyphicon glyphicon-book"></span>
         Notes
       </a>
