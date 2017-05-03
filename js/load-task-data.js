94736413bc0e70b548d3f863108dd5dd6bab6da4
Caitlin Cassidy <ccassidy@mit.edu>
2017-05-03 13:54:47 -0400

    still working on local storage

--- js/load-task-data.js
+++ js/load-task-data.js
@@ -2,44 +2,44 @@
 
 // Outstanding
 var task1 = {
-  task: "Email John about reserving space for final exam",
+  text: "Email John about reserving space for final exam",
   due: moment("04/30/2017").format(),
 }
 
 var task2 = {
-  task: "Make the beta for Quiz 1",
+  text: "Make the beta for Quiz 1",
   due: moment("05/01/2017").format(),
 }
 
 // Due today
 var task3 = {
-  task: "Ask TAs when they're available to host a Quiz 1 review session",
+  text: "Ask TAs when they're available to host a Quiz 1 review session",
   due: moment("05/03/2017").format(),
 }
 
 var task4 = {
-  task: "Pick up quiz materials from CopyTech",
+  text: "Pick up quiz materials from CopyTech",
   due: moment("05/03/2017").format(),
 }
 
 var task5 = {
-  task: "Remind students that Quiz 1 is coming up in two weeks",
+  text: "Remind students that Quiz 1 is coming up in two weeks",
   due: moment("05/03/2017").format(),
 }
 
 // Due later
 var task6 = {
-  task: "Send Quiz 1 beta to TAs for testing",
+  text: "Send Quiz 1 beta to TAs for testing",
   due: moment("05/04/2017").format(),
 }
 
 var task7 = {
-  task: "Review notes for Lecture 13",
+  text: "Review notes for Lecture 13",
   due: moment("05/08/2017").format(),
 }
 
 var task8 = {
-  task: "Review notes for Lecture 14",
+  text: "Review notes for Lecture 14",
   due: moment("05/15/2017").format(),
 }
 
